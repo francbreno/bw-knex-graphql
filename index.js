1d26e005a5420c4e9c1312a8ad398e1d25820bc6
Breno <francbreno@gmail.com>
2018-05-24 14:38:20 -0300

    reorganizing index code

--- index.js
+++ index.js
@@ -15,44 +15,53 @@ const entriesService = require('./services/entriesService')(crudServices);
 
 const schema = require('./api/schema');
 
+const services = {
+  usersService,
+  accountsService,
+  entriesService,
+  transactionsService,
+};
+
 const SERVER_PORT = process.env.SERVER_PORT || 3000;
+const devMode = process.env.NODE_ENV === 'development';
+
 const app = express();
 
+const graphqlMiddleware = () => {
+  const loaders = {
+    usersByIds: new Dataloader(usersService.getUsersByIdList),
+    accountsByUserIds: new Dataloader(accountsService.getAccountsByUserIdList),
+    accountsByIds: new Dataloader(accountsService.getAccountsByIdList),
+    entriesByAccountIds: new Dataloader(entriesService.getEntriesByAccountIdList),
+    transactionsByIds: new Dataloader(transactionsService.getTransactionByOriginAccountIdList),
+    transactionByEntryIds: new Dataloader(transactionsService.getTransactionByEntryIdList),
+  };
+  
+  return graphqlExpress({
+    schema,
+    context: {services, loaders},
+    debug: devMode
+  });
+}
+
 function startGraphqlServer(server) {
+  console.log(devMode);
+  const middlewares = [bodyParser.json()]
+  devMode && middlewares.push(knexLogger(db))
+  middlewares.push(graphqlMiddleware());
+
   server.use(
     '/graphql',
-    bodyParser.json(),
-    knexLogger(db),
-    (req, res) => {
-      const services = {
-        usersService,
-        accountsService,
-        entriesService,
-        transactionsService,
-      };
-      const loaders = {
-        usersByIds: new Dataloader(usersService.getUsersByIdList),
-        accountsByUserIds: new Dataloader(accountsService.getAccountsByUserIdList),
-        accountsByIds: new Dataloader(accountsService.getAccountsByIdList),
-        entriesByAccountIds: new Dataloader(entriesService.getEntriesByAccountIdList),
-        transactionsByIds: new Dataloader(transactionsService.getTransactionByOriginAccountIdList),
-        transactionByEntryIds: new Dataloader(transactionsService.getTransactionByEntryIdList),
-      };
-      graphqlExpress({
-        schema,
-        context: {services, loaders},
-        debug: true
-      }
-    )(req, res)}
+    middlewares
   );
 
   server.use(
     '/graphiql',
-    graphiqlExpress({ endpointURL: '/graphql' })
+    graphiqlExpress({endpointURL: '/graphql'})
   );
 
   server.listen(SERVER_PORT, () => {
-    console.log(`Server is up on port ${SERVER_PORT}`);
+    console.log(`Server is up and running in ${process.env.NODE_ENV} mode on port ${SERVER_PORT}`);
   });
 }
 
